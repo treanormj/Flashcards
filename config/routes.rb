8f3d518f25c1f81eeeb90322c8a489697cb21587
treanormj <treanormj@gmail.com>
2017-09-19 10:19:08 -0400

    uncommented routes flashcards#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'flashcards#index'
+  root 'flashcards#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flashcards, only: [:index, :show] do
