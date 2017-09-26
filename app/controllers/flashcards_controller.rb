9e80d2badcc578feebc31d4adf5fe55cf7828074
treanormj <treanormj@gmail.com>
2017-09-26 15:23:25 -0400

    everything works but form

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,7 +1,7 @@
 class FlashcardsController < ApplicationController
 
   def new
-    @flashcards = Flashcard.new
+    @flashcard = Flashcard.new
   end
 
   def index
@@ -9,10 +9,14 @@ class FlashcardsController < ApplicationController
   end
 
   def create
-    @flashcard
+    # params= {word:fork, picture_url: kitten.com, topic: kitchen}
+    #
+    @flashcard = Flashcard.create(flashcard_params)
+    @topic = Topic.find(params['topic'])
+    @flashcard.topic = @topic
     @flashcard = Flashcard.new(flashcard_params)
     # current_user would be once I use devise
-    # @mural.user = current_user
+    # @flashcard.user = current_user
     if @flashcard.save
       flash[:notice] = 'Mural added successfully'
       redirect_to flashcard_params(@flashcard)
@@ -25,7 +29,7 @@ class FlashcardsController < ApplicationController
   def show
     # use find when using an id. Use find_by when searching by any other way.
     @flashcard = Flashcard.find(params[:topic_id])
-    # got this from HTTParty
+    # This is re: HTTParty
     key = ENV["WORDNIK_API_KEY"]
     response = HTTParty.get("http://api.wordnik.com:80/v4/word.json/#{@flashcard.word}/audio?useCanonical=false&limit=50&api_key=#{key}")
     @audio = response.first["fileUrl"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root 'topics#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :topics, only: [:index, :show, :new, :destroy] do
-    resource :flashcards, only: [:index, :show, :destroy]
+    resources :flashcards, only: [:index, :show, :destroy]
   end
 
+  resources :flashcards, only: [:new, :create]
 
-  resources :flashcards, only: [:new]
-# this is for adding a static page
-  # get "/:page" => "static#show"
+  # this is for adding a static page
+  get "/:page" => "static#show"
 end
